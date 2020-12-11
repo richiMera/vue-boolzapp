fb427d8982926be3ac84c3134d57e9c7f55d56b4
Riccardo Ferrari <richi.f93@gmail.com>
2020-12-11 15:34:48 +0100

    layout di base

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+
+  el: "#root",
+  data: {
+    message: "",
+    intentions: ["Fare la spesa", "Fare regali Natale", "Andare in palestra", "Fare gli auguri a nonna"],
+  },
+  methods: {
+    addTodo: function () {
+      this.intentions.push(this.message);
+      this.message = "";
+    },
+    removeElement:function(index) {
+      this.intentions.splice(index, 1);
+    },
+  }
+});
