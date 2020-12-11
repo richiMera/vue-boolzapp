82ac0a46382585ad5f4c27ab0c115b0472925832
Riccardo Ferrari <richi.f93@gmail.com>
2020-12-11 16:11:04 +0100

    Milestone 1 complete

--- js/main.js
+++ js/main.js
@@ -2,16 +2,95 @@ var app = new Vue({
 
   el: "#root",
   data: {
-    message: "",
-    intentions: ["Fare la spesa", "Fare regali Natale", "Andare in palestra", "Fare gli auguri a nonna"],
+    contacts: [
+	{
+		name: 'Michele',
+		avatar: 'https://cdn.iconscout.com/icon/free/png-256/avatar-380-456332.png',
+		visible: true,
+		messages: [
+			{
+				date: '10/01/2020 15:30:55',
+				text: 'Hai portato a spasso il cane?',
+				status: 'sent'
+			},
+			{
+				date: '10/01/2020 15:50:00',
+				text: 'Ricordati di dargli da mangiare',
+				status: 'sent'
+			},
+			{
+				date: '10/01/2020 16:15:22',
+				text: 'Tutto fatto!',
+				status: 'received'
+			}
+		],
+	},
+	{
+		name: 'Fabio',
+		avatar: 'https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324.png',
+		visible: true,
+		messages: [
+			{
+				date: '20/03/2020 16:30:00',
+				text: 'Ciao come stai?',
+				status: 'sent'
+			},
+			{
+				date: '20/03/2020 16:30:55',
+				text: 'Bene grazie! Stasera ci vediamo?',
+				status: 'received'
+			},
+			{
+				date: '20/03/2020 16:35:00',
+				text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+				status: 'sent'
+			}
+		],
+	},
+	{
+		name: 'Samuele',
+		avatar: 'https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png',
+		visible: true,
+		messages: [
+			{
+				date: '28/03/2020 10:10:40',
+				text: 'La Marianna va in campagna',
+				status: 'received'
+			},
+			{
+				date: '28/03/2020 10:20:10',
+				text: 'Sicuro di non aver sbagliato chat?',
+				status: 'sent'
+			},
+			{
+				date: '28/03/2020 16:15:22',
+				text: 'Ah scusa!',
+				status: 'received'
+			}
+		],
+	},
+	{
+		name: 'Luisa',
+		avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSosTxC4IeaFI_pZEUi3snN4517xvVnNRjaWw&usqp=CAU',
+		visible: true,
+		messages: [
+			{
+				date: '10/01/2020 15:30:55',
+				text: 'Lo sai che ha aperto una nuova pizzeria?',
+				status: 'sent'
+			},
+			{
+				date: '10/01/2020 15:50:00',
+				text: 'Si, ma preferirei andare al cinema',
+				status: 'received'
+			}
+		],
+	},
+]
+
+
   },
   methods: {
-    addTodo: function () {
-      this.intentions.push(this.message);
-      this.message = "";
-    },
-    removeElement:function(index) {
-      this.intentions.splice(index, 1);
-    },
+
   }
 });
